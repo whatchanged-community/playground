bda4c6472eac5f9c129db54c145f1961c3fefb55
Axetroy <axetroy.dev@gmail.com>
2022-01-22 18:55:31 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import vue from "@vitejs/plugin-vue";
 import httpImport from "import-http/rollup";
 import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/" : "/",
   root: __dirname,
-  plugins: [vue(), httpImport()],
+  plugins: [
+    vue(),
+    httpImport(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
 });
